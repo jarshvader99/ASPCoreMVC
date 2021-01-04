4bac3ade4ffdd5e512844f4411726ca6c82b64d1
jarshvader99 <jarshvader@gmail.com>
2021-01-03 22:52:31 -0600

    working on view more pages and details card for businesses. need to make view more pages match styles now that the price is being pulled in and the modal exists.

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -114,6 +114,7 @@ $(".clear-results").click(function () {
         '<i class= "fas fa-utensils fa-9x search-icon" ></i>' +
         '</div>');
     $('#searchInput').val('');
+    $('#viewNext').toggleClass('hidden');
 });
 
 $(".asc").click(function () {
@@ -127,6 +128,7 @@ $(".dsc").click(function () {
 });
 
 $("#btnSearch").click(function () {
+    
     var q = $('#searchInput').val();
     $('.icon-wrapper').hide();
     var distance = $('#distance').val();
@@ -141,8 +143,6 @@ $("#btnSearch").click(function () {
     else {
         order = '';
     }
-
-    console.log(distance);
     var ulCities = $('#ulCities');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(latsAndLons);
@@ -152,8 +152,6 @@ $("#btnSearch").click(function () {
     function latsAndLons(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        console.log(latitude);
-        console.log(longitude);
         var restaurants = $('#restaurants');
         $.ajax({
             type: 'GET',
@@ -162,6 +160,86 @@ $("#btnSearch").click(function () {
             data: JSON.stringify(),
             success: function (data) {
                 restaurants.empty();
+                var i = 0;
+                $.each(data.restaurants, function (key, val) {
+                    if (data.restaurants[i].restaurant.price_range) {
+                        var price = data.restaurants[i].restaurant.price_range;
+                        price = calcPrice(price);                                             
+                    }
+                   
+                    restaurants.append(
+                        '<h3 class="card-header"><a class="restaurant-details" id="' + data.restaurants[i].restaurant.id + '" data-toggle="modal" data-target="#resDetailModal">' + data.restaurants[i].restaurant.name + '</a></h3>' +
+                        '<div class="card-body">' +
+                    
+                        '<h5 class="card-title">Rating: ' + data.restaurants[i].restaurant.user_rating.aggregate_rating + ' of 5</h5>' +
+                        '<h5 class="card-title">Price: <span style="cursor:pointer" data-toggle="tooltip" title="Average cost for two: $' + data.restaurants[i].restaurant.average_cost_for_two + '">' + price + '</span></h5>' +
+                        '<h6 class="card-subtitle text-muted">' + data.restaurants[i].restaurant.cuisines + '</h6>' +
+                        '</div>' +
+                        '<svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">' +
+                        '<rect width="100%" height="100%" fill="#868e96"></rect>' +
+                        '<text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>' +
+                        '</svg>' +
+                        '<div class="card-body">' +
+                        '<p class="card-text">' + data.restaurants[i].restaurant.highlights + '</p>' +
+                        '</div>' +
+                        
+                        '<div class="card-body">' +
+                        '<a href="' + data.restaurants[i].restaurant.menu_url + '" class="card-link" target="_blank">View Menu</a>' +
+                        '</div>' +
+                        '<div class="card-footer mb-5">' +
+                        i +
+                        '</div>');
+                    i++;
+                });
+
+
+            }
+        });
+    }
+    setTimeout(function () {
+        $('#viewNext').removeClass('hidden');
+    }, 2000)
+});
+
+$("#viewNext").click(function () {
+    var start = $('.card-footer:last').text();
+    start = parseInt(start);
+    start++;
+    var count = 20;
+    var index = $('.card-footer:last').text();
+    index = parseInt(index);
+    index++;
+    var q = $('#searchInput').val();
+    $('.icon-wrapper').hide();
+    var distance = $('#distance').val();
+    var sort = $('#sort').val();
+    var order = '';
+    if ($('.asc').hasClass('selected')) {
+        order = 'asc';
+    }
+    else if ($('.dsc').hasClass('selected')) {
+        order = 'desc';
+    }
+    else {
+        order = '';
+    }
+    var ulCities = $('#ulCities');
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(latsAndLons);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    function latsAndLons(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var restaurants = $('#restaurants');
+        $.ajax({
+            type: 'GET',
+            url: 'https://developers.zomato.com/api/v2.1/search?q=' + q + '&start=' + start + '&count=' + count + '&lat=' + latitude + '&lon=' + longitude + '&radius=' + distance + '&sort=' + sort + '&order=' + order + '&apikey=8e433de61020f20ecdcc1389db0f22a4',
+            dataType: 'json',
+            data: JSON.stringify(),
+            success: function (data) {
+                
                 var i = 0;
                 $.each(data.restaurants, function (key, val) {
 
@@ -186,33 +264,65 @@ $("#btnSearch").click(function () {
                         '<div class="card-body">' +
                         '<a href="' + data.restaurants[i].restaurant.menu_url + '" class="card-link" target="_blank">View Menu</a>' +
                         '</div>' +
-                        '<div class="card-footer text-muted mb-5">' +
+                        '<div class="card-footer mb-5">' +
+                            index +
                         '</div>');
                     i++;
+                    index++;
                 });
-
-
             }
         });
     }
 });
 
-//function filterCities() {
-//    // Declare variables
-//    var input, filter, ul, li, a, i, txtValue;
-//    input = document.getElementById('searchCityInput');
-//    filter = input.value.toUpperCase();
-//    ul = document.getElementById("ulCities");
-//    li = ul.getElementsByTagName('li');
+$(document).on("click", ".restaurant-details", function (e) {
+    var resId = $(this).attr('id');
+    var title = $('#modal-title');
+    var businessDetails = $('#business-details');
+    var thumb = $('#thumb');
+    $.ajax({
+        type: 'GET',
+        url: 'https://developers.zomato.com/api/v2.1/restaurant?res_id=' + resId + '&apikey=8e433de61020f20ecdcc1389db0f22a4',
+        dataType: 'json',
+        success: function (data) {
+            title.empty();
+            businessDetails.empty();
+            thumb.empty();  
+            title.append(data.name);
+            businessDetails.append(
+                '<ul class="list-group list-group-flush">' +
+                '<li class="list-group-item"><i class="fas fa-location-arrow"></i> ' + data.location.address + '</li>' +
+                '<li class="list-group-item"><i class="fas fa-cocktail"></i> ' + data.timings + '</li>' +
+                '<li class="list-group-item"><i class="fas fa-phone-square"></i> ' + data.phone_numbers + '</li>' +
+                '</ul>'
+            );
+            thumb.append('<img src="' + data.featured_image + '"/>')
+            $("#resDetailModal").modal('show');
+        }
+    });
+});
 
-//    // Loop through all list items, and hide those who don't match the search query
-//    for (i = 0; i < li.length; i++) {
-//        a = li[i].getElementsByTagName("a")[0];
-//        txtValue = a.textContent || a.innerText;
-//        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//            li[i].style.display = "";
-//        } else {
-//            li[i].style.display = "none";
-//        }
-//    }
-//}
+function calcPrice(price) {
+    switch (price) {
+        case 1:
+            // code block
+            price = "$";
+            break;
+        case 2:
+            // code block
+            price = "$$";
+            break;
+        case 3:
+            // code block
+            price = "$$$";
+            break;
+        case 4:
+            // code block
+            price = "$$$$";
+            break;
+        default:
+            // code block
+            price = "$";
+    } 
+    return price;
+}
